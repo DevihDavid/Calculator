52363296e5c3effee5d0d779b0511c4f41dbb6d7
David <devih05@gmail.com>
2018-02-06 21:05:05 +0100

    scientific calculator added, some bug fixes in error management

--- calc.py
+++ calc.py
@@ -1,11 +1,38 @@
 from tkinter import *
 import functions
+from math import e, pi
 
 
 class Calculator(functions.Mixin):
 
     def __init__(self, master):
         self.master = master
+
+        top_menu = Menu(master)
+        master.config(menu=top_menu)
+
+        calc_type = Menu(top_menu)
+        top_menu.add_cascade(label="Calculator", menu=calc_type)
+        calc_type.add_command(label="Simple", command=self.simple_calc)
+        calc_type.add_command(label="Scientific", command=self.scientific_calc)
+        calc_type.add_command(label="Date calculation")
+
+        converter = Menu(top_menu)
+        top_menu.add_cascade(label="Converter", menu=converter)
+        converter.add_command(label="Currency")
+        converter.add_separator()
+        converter.add_command(label="Volume")
+        converter.add_command(label="Length")
+        converter.add_command(label="Weight and mass")
+        converter.add_command(label="Temperature")
+        converter.add_command(label="Energy")
+        converter.add_command(label="Area")
+        converter.add_command(label="Speed")
+        converter.add_command(label="Time")
+        converter.add_command(label="Power")
+        converter.add_command(label="Pressure")
+        converter.add_command(label="Angle")
+        converter.add_command(label="Data")
 
         self.write_area = Entry(master, justify=RIGHT)
         self.write_area.grid(row=0, columnspan=6, sticky=W + E, ipady=6, pady=6)
@@ -20,9 +47,11 @@ class Calculator(functions.Mixin):
         self.result_area['yscrollcommand'] = self.scrollbarY.set
         self.result_area['xscrollcommand'] = self.scrollbarX.set
 
-        self.result_area_text = Label(master, text="The result is:").grid(row=0, column=6, sticky=W + E)
+        self.result_area_text = Label(master, text="The result is:")
+        self.result_area_text.grid(row=0, column=6, sticky=W + E)
 
-        self.type = Label(master, text="Simple version").grid(row=5, column=0, columnspan=6)
+        self.type = Label(master, text="Simple version")
+        self.type.grid(row=5, column=0, columnspan=6)
 
         self.button0 = self.create_and_place_button_input("0", 4, 1)
         self.button1 = self.create_and_place_button_input("1", 3, 0)
@@ -110,6 +139,76 @@ class Calculator(functions.Mixin):
             self.result_area.xview_scroll(howMany, units)
         elif op == 'moveto':
             self.result_area.xview_moveto(howMany)
+
+    def scientific_calc(self):
+        buttons_to_remove = [self.buttonBackspace, self.buttonC, self.buttonCE, self.type, self.result_area_text]
+
+        for name in buttons_to_remove:
+            name.grid_remove()
+
+        def create_and_place_button_sc(value, row, column, command):
+            button = Button(text=value, height=2, width=8, command=command)
+            button.grid(row=row, column=column)
+            return button
+
+        self.buttonN = create_and_place_button_sc("n!", 1, 5, self.c_factorial)
+        self.buttonE = create_and_place_button_sc("eˣ", 2, 5, self.exponent)
+        self.button10 = create_and_place_button_sc("10ˣ", 3, 5, self.raise_10)
+        self.buttonlogx = create_and_place_button_sc("logx", 1, 6, self.c_log)
+        self.buttonln = create_and_place_button_sc("ln", 2, 6, self.c_ln)
+        self.button_put_e = create_and_place_button_sc("e", 3, 6, lambda: self.write_area.insert(END, str(e)))
+        self.buttonP = create_and_place_button_sc("Π", 4, 6, lambda: self.write_area.insert(END, str(pi)))
+        self.buttonSin = create_and_place_button_sc("sin", 1, 7, self.c_sin)
+        self.buttonCos = create_and_place_button_sc("cos", 2, 7, self.c_cos)
+        self.buttonTg = create_and_place_button_sc("tg", 3, 7, self.c_tg)
+        self.buttonCtg = create_and_place_button_sc("ctg", 4, 7, self.c_ctg)
+        self.buttonArcsin = create_and_place_button_sc("arcsin", 1, 8, self.c_arcsin)
+        self.buttonArccos = create_and_place_button_sc("arccos", 2, 8, self.c_arcctg)
+        self.buttonArctg = create_and_place_button_sc("arctg", 3, 8, self.c_arctg)
+        self.buttonArcctg = create_and_place_button_sc("arcctg", 4, 8, self.c_arcctg)
+        self.buttonBackspace = self.create_and_place_button_action("←", 1, 9, self.clear_last)
+        self.buttonC = self.create_and_place_button_action("C", 2, 9, lambda: self.write_area.delete(0, END))
+        self.buttonCE = self.create_and_place_button_action("CE", 3, 9, self.clear)
+
+        self.scrollbarY.grid(row=1, rowspan=4, column=11, sticky=N + S)
+        self.scrollbarX.grid(row=5, column=10, sticky=W + E)
+
+        self.result_area.grid(row=1, rowspan=4, column=10)
+
+        self.result_area_text = Label(text="The result is:")
+        self.result_area_text.grid(row=0, column=10, sticky=W + E)
+
+        self.type = Label(text="Scientific version", anchor=W)
+        self.type.grid(row=5, column=4, columnspan=6)
+
+        self.write_area.grid(row=0, columnspan=10, sticky=W + E, ipady=6, pady=6)
+
+    def simple_calc(self):
+        buttons_to_remove = [self.buttonN, self.buttonE, self.button10, self.buttonlogx, self.buttonln,
+                             self.button_put_e, self.buttonP, self.buttonSin, self.buttonCos, self.buttonTg,
+                             self.buttonCtg, self.buttonArcsin, self.buttonArccos, self.buttonArctg,
+                             self.buttonArcctg, self.buttonBackspace, self.buttonC, self.buttonCE, self.type]
+
+        for name in buttons_to_remove:
+            name.grid_remove()
+
+        self.type = Label(text="Simple version", anchor=W)
+        self.type.grid(row=5, column=0, columnspan=6)
+        self.buttonBackspace = self.create_and_place_button_action("←", 1, 5, self.clear_last)
+        self.buttonC = self.create_and_place_button_action("C", 2, 5, lambda: self.write_area.delete(0, END))
+        self.buttonCE = self.create_and_place_button_action("CE", 3, 5, self.clear)
+
+        self.scrollbarY.grid(row=1, rowspan=4, column=7, sticky=N + S)
+        self.scrollbarX.grid(row=5, column=6, sticky=W + E)
+
+        self.result_area.grid(row=1, rowspan=4, column=6)
+
+        self.result_area_text.grid(row=0, column=6, sticky=W + E)
+
+        self.type = Label(text="Simple version", anchor=W)
+        self.type.grid(row=5, column=0, columnspan=6)
+
+        self.write_area.grid(row=0, columnspan=6, sticky=W + E, ipady=6, pady=6)
 
 
 root = Tk()
--- functions.py
+++ functions.py
@@ -12,8 +12,9 @@ class Mixin:
 
     def equal(self, event=None):
         equation = self.write_area.get()
-        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-*/.]{2,}', equation) \
-                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation):
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
             self.error()
             self.write_area.delete(0, END)
         else:
@@ -32,8 +33,9 @@ class Mixin:
 
     def square(self):
         equation = self.write_area.get()
-        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-*/.]{2,}', equation) \
-                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation):
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
             self.error()
             self.write_area.delete(0, END)
         else:
@@ -44,8 +46,9 @@ class Mixin:
 
     def squareY(self, event=None):
         equation = self.write_area.get()
-        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-*/.]{2,}', equation) \
-                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation):
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
             self.error()
             self.write_area.delete(0, END)
         else:
@@ -61,8 +64,9 @@ class Mixin:
 
     def sqrt(self):
         equation = self.write_area.get()
-        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-*/.]{2,}', equation) \
-                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation):
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
             self.error()
             self.write_area.delete(0, END)
         else:
@@ -70,3 +74,174 @@ class Mixin:
             self.result_area.insert(1.0, "√(" + self.write_area.get() + ")=" + str(sqrt(number)) + "\n")
             self.write_area.delete(0, END)
             self.write_area.insert(0, str(number))
+
+    def c_factorial(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = factorial(eval(compile(equation, '<string>', 'eval')))
+            self.result_area.insert(1.0, self.write_area.get() + "!=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def exponent(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = exp(eval(compile(equation, '<string>', 'eval')))
+            self.result_area.insert(1.0, "e**"+ equation + "=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def raise_10(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = 10 ** eval(compile(equation, '<string>', 'eval'))
+            self.result_area.insert(1.0, "10**" + equation + "=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_log(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = log10(eval(compile(equation, '<string>', 'eval')))
+            self.result_area.insert(1.0, "log(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_ln(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = log((eval(compile(equation, '<string>', 'eval'))), e)
+            self.result_area.insert(1.0, "ln(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_sin(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = sin((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "sin(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_cos(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = cos((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "cos(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_tg(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = tg((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "tg(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_ctg(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation):
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = ctg((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "ctg(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_arcsin(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation) or float(equation) >= -1 or float(equation) <= 1:
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = asin((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "arcsin(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_arccos(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation) or float(equation) >= -1 or float(equation) <= 1:
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = acos((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "arccos(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_arctg(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation) or float(equation) >= -1 or float(equation) <= 1:
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = atg((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "arctg(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+    def c_arcctg(self):
+        equation = self.write_area.get()
+        if re.search('[a-zA-Z,]+', equation) or equation == "" or re.search('[+\-/.]{2,}', equation) \
+                or re.search('^[+\-*/.]', equation) or re.search('[+\-*/.]$', equation) \
+                or re.search('[*]{3,}', equation) or float(equation) >= -1 or float(equation) <= 1:
+            self.error()
+            self.write_area.delete(0, END)
+        else:
+            result = acos((eval(compile(equation, '<string>', 'eval'))))
+            self.result_area.insert(1.0, "arccos(" + equation + ")=" + str(result) + "\n")
+            self.write_area.delete(0, END)
+            self.write_area.insert(0, str(result))
+
+
